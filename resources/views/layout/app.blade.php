39648d3afe8c9d54f432ecc04b5f9c0dcf7cb850
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2021-03-09 12:28:33 -0800

    Fixed wrong lang placement

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<html>
-    <head lan="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Conex - @yield("title")</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
